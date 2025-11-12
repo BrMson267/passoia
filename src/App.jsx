5b4966bf61aa3848a63bbf5001896a919bbef3f6
Bruno Martins Silva Oliveira do Nascimento <bruno.martinsson@outlook.com>
2025-11-12 19:23:49 -0300

    Novo projeto passoia

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+
+function App () {
+  return(
+    <>
+      <Header/>
+      <h1>Passoia</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi pariatur debitis animi eius, dolores ad enim necessitatibus reiciendis harum fugiat! Nam possimus odit tempora natus doloremque neque vero eos asperiores?</p>
+    
+    
+    
+    </>
+  );
+}
+export default App;
+
+
